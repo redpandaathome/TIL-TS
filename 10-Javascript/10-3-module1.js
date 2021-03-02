d11d41f3377feece9420b0a6a2e829ec59289ef0
redpandaathome <leeyumi415@gmail.com>
2021-03-02 14:39:26 +0900

    10-3-module

--- 10-Javascript/10-3-module1.js
+++ 10-Javascript/10-3-module1.js
@@ -1,3 +1,15 @@
-function add(a,b) {
+//defaul로 export시 다른 파일에서 아무이름으로 불러올 수 있다.
+// export default function add(a,b) {
+//    return a+b;
+// }
+
+export function add(a,b) {
    return a+b;
 }
+
+//한파일 내에서 두가지 "default" export를 쓸 순 없다.
+export function print(){
+   console.log("printing...🖨")
+}
+
+export const number = 10;
--- 10-Javascript/10-3-module2.js
+++ 10-Javascript/10-3-module2.js
@@ -1,3 +1,21 @@
-console.log(add(1,2))
+// import anyName, {print as printMessage} from './10-3-module1.js';
+//혹은 default가 없다면
+import * as calculator from './10-3-module1.js'
+console.log(calculator.add(1,2))
+calculator.print();
+//변수도 가능
+console.log(calculator.number);
+
+
+// console.log(anyName(1,2))
 //3이 뜬다...! 모듈화를 하지 않아서 글로벌 스코프이기에.
 //window.add(1,3)
+
+//type="module" 작성한 후에는 두 파일은 서로 접근x
+
+//default가 아닌경우 {정확한이름}으로 임포트 또는 {정확한이름 as 원하는이름}
+// printMessage();
+
+
+
+//✨모듈화>파일간 이름 충돌 방지, 코드분리 ->모듈성업, 모듈간 재사용성도 업
