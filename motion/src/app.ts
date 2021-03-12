8288770a2ef8895ef750033f65894747d2261aef
redpandaathome <leeyumi415@gmail.com>
2021-03-12 17:26:38 +0900

    motion-imageComponent

--- motion/src/app.ts
+++ motion/src/app.ts
@@ -1,10 +1,17 @@
-import { PageComponent } from "./components/page.js";
+import { ImageComponent } from "./components/page/item/image.js";
+import { PageComponent } from "./components/page/page.js";
 
 class App {
    private readonly page: PageComponent;
+   // private readonly image: ImageComponent;
    constructor(appRoot: HTMLElement){
       this.page = new PageComponent();
       this.page.attachTo(appRoot);
+      
+      // this.image = new ImageComponent('https://picsum.photos/600/300','title🌼');
+      // this.image.attachTo(appRoot);
+      const image = new ImageComponent('https://picsum.photos/600/300','title🌼');
+      image.attachTo(appRoot, 'beforeend')
    }
 }
 
---
+++ motion/src/components/page/item/image.ts
@@ -0,0 +1,24 @@
+export class ImageComponent {
+   private element: HTMLElement;
+   constructor(url:string, title:string){
+      const template = document.createElement('template');
+      template.innerHTML = 
+         `<section class="image">
+            <div class="image__holder">
+               <img src="" alt="" class="img__thumbnail">
+               <p class="image__title"></p>
+            </div>
+         </section>`
+      this.element = template.content.firstElementChild! as HTMLElement;
+
+      const imageElement = this.element.querySelector('.img__thumbnail')! as HTMLImageElement;
+      imageElement.src = url;
+      imageElement.alt = title;
+      const titleElement = this.element.querySelector('.image__title')! as HTMLParagraphElement;
+      titleElement.textContent = title;
+   }
+
+   attachTo(parent: HTMLElement, position: InsertPosition = 'afterbegin') {
+      parent.insertAdjacentElement(position, this.element)
+   }
+}
