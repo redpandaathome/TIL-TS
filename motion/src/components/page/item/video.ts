caccd5f974e7bbe5f1f3f650d654b27b32221fa7
redpandaathome <leeyumi415@gmail.com>
2021-03-16 09:47:45 +0900

    videoComponent - regExp

--- motion/src/components/page/item/video.ts
+++ motion/src/components/page/item/video.ts
@@ -25,15 +25,26 @@ export class VideoComponent extends BaseComponent<HTMLElement>{
 
    //Output
    //"https://www.youtube.com/embed/qhyUrb9LHpY"
+   // 기존코드
+   // private convertToEmbeddedURL(url:string):string {
+   //    const youTubeUrl = 'https://www.youtube.com/embed/';
+   //    if(url.includes('/youtu.be/')){
+   //       url = url.substring(url.lastIndexOf("/youtu.be/") + "/youtu.be/".length);
+   //    } else if(url.includes('?v=')){
+   //       url = url.substring(url.lastIndexOf("?v=") + "?v=".length);
+   //    }
+   //    return youTubeUrl+url;
+   // }
    // 정규표현식 Regex
    private convertToEmbeddedURL(url:string):string {
-      const youTubeUrl = 'https://www.youtube.com/embed/';
-      if(url.includes('/youtu.be/')){
-         url = url.substring(url.lastIndexOf("/youtu.be/") + "/youtu.be/".length);
-      } else if(url.includes('?v=')){
-         url = url.substring(url.lastIndexOf("?v=") + "?v=".length);
+      const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/
+      const match = url.match(regExp);
+
+      const videoId = match ? match[1] || match[2] : undefined;
+      if(videoId) {
+         return `https://www.youtube.com/embed/${videoId}`;
       }
-      return youTubeUrl+url;
+      return url;
    }
 }
 
