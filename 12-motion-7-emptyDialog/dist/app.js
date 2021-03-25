6422f6b19b112bec77301242d43c3d3b335618fd
redpandaathome <leeyumi415@gmail.com>
2021-03-25 21:20:41 +0900

    12-7.emptyDialog DONE

--- 12-motion-7-emptyDialog/dist/app.js
+++ 12-motion-7-emptyDialog/dist/app.js
@@ -3,6 +3,7 @@ import { PageComponent, PageItemComponent } from './components/page/page.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
+import { InputDialog } from './components/dialog/dialog.js';
 var App = /** @class */ (function () {
     function App(appRoot) {
         this.page = new PageComponent(PageItemComponent);
@@ -15,6 +16,18 @@ var App = /** @class */ (function () {
         this.page.addChild(todo);
         var video = new VideoComponent('video-title', "https://www.youtu.be/G7UEUf5KAvI");
         this.page.addChild(video);
+        var imageBtn = document.querySelector("#new-image");
+        imageBtn.addEventListener("click", function () {
+            var dialog = new InputDialog();
+            // const dialogBody = document.querySelector('#dialog__body')! as HTMLElement;
+            dialog.setOnCloseListener(function () {
+                dialog.removeFrom(document.body);
+            });
+            dialog.setOnSubmitListener(function () {
+                dialog.removeFrom(document.body);
+            });
+            dialog.attachTo(document.body);
+        });
     }
     return App;
 }());
--- 12-motion-7-emptyDialog/src/app.ts
+++ 12-motion-7-emptyDialog/src/app.ts
@@ -4,6 +4,7 @@ import { PageComponent, PageItemComponent, Composable } from './components/page/
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
 import { VideoComponent } from './components/page/item/video.js';
+import { InputDialog } from './components/dialog/dialog.js';
 
 class App {
    // private readonly page:PageComponent;
@@ -24,6 +25,21 @@ class App {
       
       const video = new VideoComponent('video-title', "https://www.youtu.be/G7UEUf5KAvI")
       this.page.addChild(video)
+
+      const imageBtn = document.querySelector("#new-image")! as HTMLButtonElement;
+      imageBtn.addEventListener("click", ()=>{
+         const dialog = new InputDialog();
+         // const dialogBody = document.querySelector('#dialog__body')! as HTMLElement;
+         dialog.setOnCloseListener(()=>{
+            dialog.removeFrom(document.body)
+         });
+
+         dialog.setOnSubmitListener(()=>{
+            dialog.removeFrom(document.body)
+         });
+
+         dialog.attachTo(document.body);
+      })
    }
 }
 
