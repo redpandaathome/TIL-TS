40d3fc891a3bbcbf0a8f7e073f9f6b63d3273c71
redpandaathome <leeyumi415@gmail.com>
2021-03-29 17:50:44 +0900

    ...ing

--- 12-motion-8-Dialog/src/components/dialog/dialog.ts
+++ 12-motion-8-Dialog/src/components/dialog/dialog.ts
@@ -36,6 +36,7 @@ export class InputDialog extends BaseComponent<HTMLElement> implements Composabl
    }
 
    addChild(child:Component){
-      
+      const body = this.element.querySelector("#dialog__body")! as HTMLElement;
+      child.attachTo(body);
    }
 }
