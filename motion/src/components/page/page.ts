698178a8d24a32ea11470d1743ac356719e460f5
redpandaathome <leeyumi415@gmail.com>
2021-04-08 23:19:31 +0900

    motion drag2 - DONE

--- motion/src/components/page/page.ts
+++ motion/src/components/page/page.ts
@@ -38,18 +38,25 @@ export class PageComponent extends BaseComponent<HTMLUListElement> implements Co
       item.setOnCloseListener(()=>{
          item.removeFrom(this.element)
       })
+      item.setOnDragStateListener((target: SectionContainer, state: DragState) => {
+         console.log(target, state)
+      })
    }
 
 }
 
 type OnCloseListener = () => void
+type DragState = 'start' | 'end' | 'enter' | 'leave';
+type OnDragStateListener<T extends Component> = (target: T, state: DragState) => void;
 
 interface SectionContainer extends Component, Composable {
    setOnCloseListener(listener:OnCloseListener):void
+   setOnDragStateListener(listener: OnDragStateListener<SectionContainer>):void;
 }
 
 export class PageItemComponent extends BaseComponent<HTMLLIElement> implements SectionContainer {
    private closeListener?: OnCloseListener; //✨
+   private dragStateListener?: OnDragStateListener<PageItemComponent>;
    constructor(){
       super(
          `<li draggable="true" class="page-item">
@@ -65,28 +72,53 @@ export class PageItemComponent extends BaseComponent<HTMLLIElement> implements S
       }
 
       this.element.addEventListener('dragstart', (event: DragEvent)=> {
-         this.onDragStrat(event)
+         this.onDragStart(event)
       })
 
       this.element.addEventListener('dragend', (event: DragEvent)=> {
          this.onDragEnd(event)
       })
 
+      this.element.addEventListener('dragenter', (event: DragEvent)=> {
+         this.onDragEnter(event)
+      })
+
+      this.element.addEventListener('dragleave', (event: DragEvent)=> {
+         this.onDragLeave(event)
+      })
+
    }
 
-   onDragStrat(event: DragEvent) {
-      console.log('dragStart', event)
+   onDragStart(_: DragEvent) {
+      this.notifyDragObservers('start');
    }
 
-   onDragEnd(event: DragEvent) {
-      console.log('dragEnd', event)
+   onDragEnd(_: DragEvent) {
+      this.notifyDragObservers('end');
+   }
+
+   onDragEnter(_: DragEvent) {
+      this.notifyDragObservers('enter');
+   }
+
+   onDragLeave(_: DragEvent) {
+      this.notifyDragObservers('leave');
+   }
+
+   notifyDragObservers(state: DragState){
+      this.dragStateListener && this.dragStateListener(this, state);
    }
 
    addChild(child:Component){
       const item = this.element.querySelector('.page-item__body')! as HTMLElement;
       child.attachTo(item);
    }
+   
    setOnCloseListener(listener:OnCloseListener){
       this.closeListener = listener; //전달받은 리스너로 등록
+   }
+
+   setOnDragStateListener(listener: OnDragStateListener<PageItemComponent>) {
+      this.dragStateListener = listener;
    }
 }
